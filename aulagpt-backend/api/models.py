aa1dc3a6c9b1afeaae676048a2ae90d2cf0eb9e4
Hugo <hugoagreda22@gmail.com>
2025-06-05 12:29:15 +0200

    Mensaje que describa tus cambios

--- aulagpt-backend/api/models.py
+++ aulagpt-backend/api/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+from .usr import User
 
 from django.contrib.auth import get_user_model
 User = get_user_model()
