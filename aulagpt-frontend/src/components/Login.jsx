596b539974f777279fa1a92e05de3319ef10c18d
Hugo <hugoagreda22@gmail.com>
2025-06-12 18:46:11 +0200

    Mensaje que describa tus cambios

--- aulagpt-frontend/src/components/Login.jsx
+++ aulagpt-frontend/src/components/Login.jsx
@@ -41,8 +41,6 @@ function Login() {
           // Si no se devuelve el rol, puedes redirigir por defecto
           navigate("./TeacherDashboard");
         }
-
-        alert("Login correcto");
         console.log("Token de acceso:", data.access);
       } else {
         setErrorMsg(data.detail || "Error al iniciar sesión");
