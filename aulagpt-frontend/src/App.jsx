69f1ed4b03ff1a9ff4435c289ede8f5f26bd3b53
Hugo <hugoagreda22@gmail.com>
2025-06-12 17:40:48 +0200

    Mensaje que describa tus cambios

--- aulagpt-frontend/src/App.jsx
+++ aulagpt-frontend/src/App.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./components/Home";
-import Login from "./components/Login";
-import Register from "./components/Register";
 import ChatIA from "./components/ChatIA";
 
 // Ruta protegida que verifica token
 function PrivateRoute({ children }) {
   const token = localStorage.getItem("token");
-  return token ? children : <Navigate to="/login" />;
+  return token ? children : <Navigate to="/" />;
 }
 
 function App() {
@@ -18,16 +16,15 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route 
-            path="/chat" 
+          <Route
+            path="/chat"
             element={
               <PrivateRoute>
                 <ChatIA />
               </PrivateRoute>
-            } 
+            }
           />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </div>
     </Router>
