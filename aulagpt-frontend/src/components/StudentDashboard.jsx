5750a5229602c62ccd8bb6c9de9ede388840b7dc
Hugo <hugoagreda22@gmail.com>
2025-06-12 18:42:33 +0200

    Mensaje que describa tus cambios

--- aulagpt-frontend/src/components/StudentDashboard.jsx
+++ aulagpt-frontend/src/components/StudentDashboard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/dashboard.css";
+import "../styles/Dashboard.css";
 
 const StudentDashboard = () => {
   return (
--- aulagpt-frontend/src/components/TeacherDashboard.jsx
+++ aulagpt-frontend/src/components/TeacherDashboard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/dashboard.css";
+import "../styles/Dashboard.css";
 
 const TeacherDashboard = () => {
   return (
