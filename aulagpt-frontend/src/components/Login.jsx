c7d837e5d8c7138777dace704ba1c43639b6ed75
Hugo <hugoagreda22@gmail.com>
2025-06-14 13:15:49 +0200

    Mensaje que describa tus cambios

--- aulagpt-frontend/src/components/Login.jsx
+++ aulagpt-frontend/src/components/Login.jsx
@@ -20,7 +20,7 @@ function Login() {
     setMessageType("");
 
     try {
-      const response = await fetch("https://aulagpt.onrender.com/api/", {
+      const response = await fetch("https://aulagpt.onrender.com/api/users/login/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
